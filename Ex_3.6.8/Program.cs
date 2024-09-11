59c197530bb054972ba94b206556b0b4c4a6eeb4
Влад Зиненко <deadbeat686@gmail.com>
2024-09-11 17:40:35 +0300

    #

---
+++ Ex_3.6.8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_3._6._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 10 % 3;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ex_3.7.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fin_Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите ваш возраст: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Введите ваш день, месяц и год рождения");
+            byte dayOfBirth = byte.Parse(Console.ReadLine());
+            byte monthOfBirth = byte.Parse(Console.ReadLine());
+            int yearOfBirth = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Ваше имя: {name} \n" +
+                $"Ваш возраст: {age} \n" +
+                $"Ваша дата рождения: {dayOfBirth}.{monthOfBirth}.{yearOfBirth}");
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
